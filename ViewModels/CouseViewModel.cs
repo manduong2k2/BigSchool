e57abfd2f5cbebe3ad42ae4d63646edef17402d2
Man <Man@DESKTOP-SPQ78SO>
2023-03-13 21:30:47 +0700

    Add project files.

---
+++ ViewModels/CouseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.ViewModels
+{
+    public class CourseViewModel
+
+    {
+        public string Place { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+    }
+
+}
